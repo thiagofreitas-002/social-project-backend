6004611251dbcfaea16f275736d76382952b0aed
Jjoao11 <njoaoneto828@gmail.com>
2022-12-06 09:46:53 -0300

    crieias migrations

---
+++ database/migrations/2022_12_06_111219_create_cardapios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cardapios', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->string('breakfest');
+            $table->string('lunch');
+            $table->string('afternoon_snack');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cardapios');
+    }
+};
