620cbb2013b3f08cfc9b58f3c9d201e343adeae1
Jjoao11 <njoaoneto828@gmail.com>
2022-12-06 15:56:26 -0300

    create migrations and admin endpoint

---
+++ app/Http/Controllers/Api/AdminController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\AdminRequest;
+use App\Models\Admin;
+
+
+class AdminController extends Controller
+{
+    protected $admin;
+    public function __contruct(Admin $admin){
+        $this->admin = $admin;
+    }
+
+    public function store(AdminRequest $request){
+        // $datas = $request->all();
+        // $insert = Admin::create($datas);
+        // return response()->json($insert);
+        
+        try{
+            $this->admin['name'] = $request->name;
+            $this->admin['email'] = $request->email;
+            $this->admin['password'] = bcrypt($request->password);
+            $this->admin['telephone'] = $request->telephone;
+            Admin::create($this->admin);
+        }catch(Exception $e){
+            abort($e);
+        }
+    }
+
+    public function destroy($id){
+        if(!$data = $this->admin->find($id)){
+            return response()->json(['error:' => 'admin não encontrado'], 404);
+        }
+        $data->delete();
+        return response()->json(['sucessful: ' => 'deletado com sucesso'], 200);
+    }
+}
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'email', 
+        'name', 
+        'password', 
+        'telephone'
+    ];
 }
