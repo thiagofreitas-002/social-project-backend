6f00b35e0f431bc80449284c2b5663daba04d0d5
Artur Bruno <arturjuliao20@gmail.com>
2022-12-10 16:20:40 -0300

    create 'get today menu' endpoint

--- app/Http/Controllers/Api/CardapioController.php
+++ app/Http/Controllers/Api/CardapioController.php
@@ -8,8 +8,24 @@ use Illuminate\Support\Facades\DB;
 
 class CardapioController extends Controller
 {
+
+    private $columnsToReturn = ['id', 'date', 'breakfest', 'lunch', 'afternoon_snack'];
+
     public function getAll() {
-        $menus = DB::table('cardapios')->get(['id', 'date', 'breakfest', 'lunch', 'afternoon_snack']);
+        $menus = DB::table('cardapios')->get($this->columnsToReturn);
         return response()->json($menus);
+    }
+
+    public function getTodayMenu() {
+        try {
+            $todayDate = date('Y/m/d');
+            $todayMenu = DB::table('cardapios')->where('date', $todayDate)->first($this->columnsToReturn);
+            if ($todayMenu == null) {
+                return response()->json(["message" => "Cardápio de hoje não cadastrado"], 502);
+            }
+            return response()->json($todayMenu);
+        } catch (Exception $e) {
+            abort($e);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\Api\NoticiasController;
 
 Route::GET('news', [NoticiasController::class, 'getAll']);
 Route::GET('menus', [CardapioController::class, 'getAll']);
+Route::GET('menus/today', [CardapioController::class, 'getTodayMenu']);
 Route::GET('news/{id}', [NoticiasController::class, 'getById']);
