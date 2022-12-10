9bd5cb2cdc211388674f36cd50bd86b39baa82cc
Artur Bruno <arturjuliao20@gmail.com>
2022-12-10 14:21:22 -0300

    remove unnecessary endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\NoticiasController;
 use App\Http\Controllers\Api\SuporteController;
-
-Route::POST('admin', [AdminController::class, 'store']);
-Route::POST('suporte', [SuporteController::class, 'store']);
