574435e77e47fb19745072092b1b980dd6f40e99
Artur Bruno <arturjuliao20@gmail.com>
2022-12-11 17:21:35 -0300

    add script to run project on docker

---
+++ run_project.sh
@@ -0,0 +1,9 @@
+docker compose down
+docker image rm arturbruno17/socialproject-backend:v1
+
+docker build --tag arturbruno17/socialproject-backend:v1 .
+docker compose up -d
+
+sleep 20
+
+docker compose exec socialproject-backend php artisan migrate
