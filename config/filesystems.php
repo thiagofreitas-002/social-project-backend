d325be0e211a000919caec917c98968bf2fa10e7
Artur Bruno <arturjuliao20@gmail.com>
2023-01-09 12:02:43 -0300

    id

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        base_path('public/storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
